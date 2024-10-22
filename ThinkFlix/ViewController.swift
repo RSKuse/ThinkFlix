5bbe0aac78dddcc345288407bffb4520d8cb95d6
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-10-22 23:32:53 +0200

    Initial Commit

---
+++ ThinkFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThinkFlix
+//
+//  Created by Reuben Simphiwe Kuse on 2024/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
